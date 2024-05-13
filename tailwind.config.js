9c77fb06a88a9482f0e72cd52925fd6a98717c93
Adrigorithm <2ndalgorithm@gmail.com>
2024-05-13 14:58:00 +0200

    tailwind and ts

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+    "pages/*.html",
+    "assets/css/*.css",
+    "assets/ts/*.ts"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
