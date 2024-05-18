b64ee3e47654a1423fe147b3103b84dc6dc3b059
Adrigorithm <2ndalgorithm@gmail.com>
2024-05-18 15:32:15 +0000

    Oh no the .git directory moved :^)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p class="text-red-700">
+        Click on the Vite and Solid logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "index.html",
-    "pages/*.html",
-    "assets/css/*.css",
-    "assets/ts/*.ts"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        paraBlue: "oklch(82.65% 0.066 250.73)",
-        paraRed: "oklch(40.42% 0.107 21.64)"
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
