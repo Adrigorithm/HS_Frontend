a446c57ffa94e87d6d5a16d8d3ccff185e8a9d08
Adriaan Waem <2ndalgorithm@gmail.com>
2024-06-20 15:50:58 +0200

    removed generated content from tracking

--- src/assets/js/greeter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greeter(name) {
-    return `Hello, ${name}!`;
-}
-let user = "Jane User";
-document.body.textContent = greeter(user);
