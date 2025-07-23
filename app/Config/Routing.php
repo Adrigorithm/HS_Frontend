f27009445c54464c60989dc90d72b211b04d0887
Adriaan Waem <2ndalgorithm@gmail.com>
2025-07-23 09:22:58 +0200

    licence is not spelled l-i-c-e-n-s-e

--- app/Config/Routing.php
+++ app/Config/Routing.php
@@ -6,7 +6,7 @@
  * (c) CodeIgniter Foundation <admin@codeigniter.com>
  *
  * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
+ * the LICENCE file that was distributed with this source code.
  */
 
 namespace Config;
