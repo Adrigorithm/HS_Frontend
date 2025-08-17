19d5bac6bd7e1df4e8a08af9828982cde958d031
Adriaan Waem <2ndalgorithm@gmail.com>
2025-08-17 14:11:49 +0200

    some landing page eyecatcher stuff

--- app/Views/content/index.php
+++ app/Views/content/index.php
@@ -8,6 +8,13 @@
             <video class="min-w-full" autoplay controls loop>
                 <source src="/assets/videos/intro.webm" type="video/webm" />
             </video>
+            <div class="flex flex-col sm:flex-row">
+                <h2 class="border-b-3 sm:border-b-0 sm:border-r-3 border-red dark:border-blue p-3 text-center text-xl font-bold">Intro</h2>
+                <div class="flex flex-col p-3">
+                    <h3 class="text-center sm:text-left font-bold">Dear visitor</h3>
+                    <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+                </div>
+            </div>
         </section>
     </main>
 
